533195a93b1f04728cbfa1428ad7c73669b998c6
Andy Wright <atwright147@gmail.com>
2018-04-11 23:27:05 +0100

    make month presets links work (ish)

--- src/scripts/search-time.js
+++ src/scripts/search-time.js
@@ -1,13 +1,17 @@
-// const selected = document.querySelector('#top_nav').querySelector('.hdtbU.hdtb-mn-o')
-// console.info(document.querySelector(selected));
 
-// $(document).ready(() => {
-//     console.info($('.hdtbU.hdtb-mn-o'));
-// });
+const timeMenuMonthItem = document.getElementById('qdr_m');
+const timeMenuMonthItemOuterHtml = timeMenuMonthItem.outerHTML;
 
-const observer = new MutationObserver((mutations) => {
-    mutations.forEach((mutation) => {
-        console.info(mutation);
-    });
-});
-observer.observe(document, { childList: true });
+const timeMenuMonthItem3 = timeMenuMonthItemOuterHtml.replace('id="qdr_m"', 'id="qdr_m3"').replace('tbs=qdr:m', 'tbs=qdr:m3').replace('Past month', 'Past 3 months');
+const timeMenuMonthItem6 = timeMenuMonthItemOuterHtml.replace('id="qdr_m"', 'id="qdr_m6"').replace('tbs=qdr:m', 'tbs=qdr:m6').replace('Past month', 'Past 6 months');
+
+timeMenuMonthItem.after(document.createRange().createContextualFragment(timeMenuMonthItem6));
+timeMenuMonthItem.after(document.createRange().createContextualFragment(timeMenuMonthItem3));
+
+console.log(timeMenuMonthItem.outerHTML);
+
+
+
+// <li class="hdtbItm" id="qdr_m">
+//     <a class="q qs" href="/search?q=google&amp;source=lnt&amp;tbs=qdr:m&amp;sa=X&amp;ved=0ahUKEwjeieH2nLPaAhXRZVAKHR2QDv4QpwUIIQ">Past month</a>
+// </li>
