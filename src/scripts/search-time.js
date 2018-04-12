fe6daaae46bb84d37aa5984b3e77e38fd4e276cb
Andy Wright <atwright147@gmail.com>
2018-04-12 21:01:31 +0100

    make page augmentation wrk more reliably (ish)

--- src/scripts/search-time.js
+++ src/scripts/search-time.js
@@ -1,16 +1,13 @@
 
-const timeMenuMonthItem = document.getElementById('qdr_m');
-const timeMenuMonthItemOuterHtml = timeMenuMonthItem.outerHTML;
+const addElements = () => { 
+    const timeMenuMonthItem = document.getElementById('qdr_m');
+    const timeMenuMonthItemOuterHtml = timeMenuMonthItem.outerHTML;
+    
+    const timeMenuMonthItem3 = timeMenuMonthItemOuterHtml.replace('id="qdr_m"', 'id="qdr_m3"').replace('tbs=qdr:m', 'tbs=qdr:m3').replace('Past month', 'Past 3 months');
+    const timeMenuMonthItem6 = timeMenuMonthItemOuterHtml.replace('id="qdr_m"', 'id="qdr_m6"').replace('tbs=qdr:m', 'tbs=qdr:m6').replace('Past month', 'Past 6 months');
+    
+    timeMenuMonthItem.after(document.createRange().createContextualFragment(timeMenuMonthItem6));
+    timeMenuMonthItem.after(document.createRange().createContextualFragment(timeMenuMonthItem3));
+};
 
-const timeMenuMonthItem3 = timeMenuMonthItemOuterHtml.replace('id="qdr_m"', 'id="qdr_m3"').replace('tbs=qdr:m', 'tbs=qdr:m3').replace('Past month', 'Past 3 months');
-const timeMenuMonthItem6 = timeMenuMonthItemOuterHtml.replace('id="qdr_m"', 'id="qdr_m6"').replace('tbs=qdr:m', 'tbs=qdr:m6').replace('Past month', 'Past 6 months');
-
-timeMenuMonthItem.after(document.createRange().createContextualFragment(timeMenuMonthItem6));
-timeMenuMonthItem.after(document.createRange().createContextualFragment(timeMenuMonthItem3));
-
-
-
-
-// <li class="hdtbItm" id="qdr_m">
-//     <a class="q qs" href="/search?q=google&amp;source=lnt&amp;tbs=qdr:m&amp;sa=X&amp;ved=0ahUKEwjeieH2nLPaAhXRZVAKHR2QDv4QpwUIIQ">Past month</a>
-// </li>
+setTimeout(addElements, 100);  // hacky but it'll do for now
